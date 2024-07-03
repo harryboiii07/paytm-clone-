683c43e4f296720343c5ac48dce60aad57b663fc
harshit <chicholikarharshit@gmail.com>
2024-07-03 17:49:44 +0530

    signin route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,6 +11,12 @@ const signupschema = zod.object({
   username : zod.string().email(),
   password : zod.string(),
 })
+
+const signinschema = zod.object({
+  username : zod.string().email(),
+  password : zod.string(),
+})
+
 userrouter.post("/signup",async (req,res) => {
   const body = req.body;
   const result = signupschema.safeParse(body);
@@ -32,11 +38,34 @@ userrouter.post("/signup",async (req,res) => {
     userid : dbuser._id
   },JWT_SECRET)
 
-  res.json({
+  res.status(200).json({
     msg : "user created successfully"
     token : token
   })
+})
 
+userrouter.post("/signin",async (req,res){
+    const body = req.body;
+    const result = signinschema.safeParse(body);
+    if(!result.success){
+      return res.status(411).json({
+        msg : "invalid inputs"
+      });
+    }
+    const user = await User.findOne({
+    username : body.username
+    })
+    if (!user){
+      return res.status(411).json({
+        msg : "user not found. signup first"
+      })
+    }
+    const token = jwt.sign({
+      userid : body._id
+    },JWT_SECRET)
+    res.status(200).json({
+      token : token
+    })
 })
 
 module.exports({
